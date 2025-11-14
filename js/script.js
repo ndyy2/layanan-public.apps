b0e7e137d1ee04aa024b7ed528b293a616404410
manz-zenzz <bintanguyaya@gmail.com>
2025-11-14 14:32:59 +0700

    r

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function cariData() {
-  const input = document.getElementById("search").value.toLowerCase();
-  const cards = document.querySelectorAll(".card");
-
-  cards.forEach((card) => {
-    const nama = card.dataset.nama.toLowerCase();
-    card.style.display = nama.includes(input) ? "block" : "none";
-  });
-}
